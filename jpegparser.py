56a16f5870962f4d86e5d17f27c7574cc592c987
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2018-02-02 00:00:17 -0300

    Refactor jpegparser into a class (NFC)
    Currently this doesn't make much sense but it simplifies the next changes,
    that will read from a byte buffer instead of a file.

--- jpegparser.py
+++ jpegparser.py
@@ -13,68 +13,76 @@ STANDALONE_MARKERS = (
     [0xd0 + n for n in range(8)] # RST markers
 )
 
-def skip_segment(fp):
-    """
-    Returns the segment type and size, but skips over the data.
-    Leaves the stream ready to read the next marker, including the 0xff.
-    """
-    print("Location: %d" % fp.tell())
-    has_ff = False
-    b = fp.read(1)[0]
-    while b == 0xff:
-        print("Saw a ff...")
-        has_ff = True
-        b = fp.read(1)[0]
+class JpegReader:
+    def __init__(self, fp):
+        self.fp = fp
 
-    if not has_ff:
-        raise RuntimeError("wtf? found %02x instead" % b)
+    def skip_segment(self):
+        """
+        Returns the segment type and size, but skips over the data.
+        Leaves the stream ready to read the next marker, including the 0xff.
+        """
+        print("Location: %d" % self.fp.tell())
+        has_ff = False
+        b = self.fp.read(1)[0]
+        while b == 0xff:
+            print("Saw a ff...")
+            has_ff = True
+            b = self.fp.read(1)[0]
 
-    marker = b
+        if not has_ff:
+            raise RuntimeError("wtf? found %02x instead" % b)
 
-    if marker in STANDALONE_MARKERS:
-        print("This is a standalone %02X marker" % marker)
-        return (marker, 0)
-    else:
-        print("This is non-standalone %02X marker, there should be a length next" % marker)
-        length_bytes = fp.read(2)
-        length = struct.unpack('>H', length_bytes)[0]-2
-        if fp.seekable:
-            fp.seek(length, io.SEEK_CUR)
+        marker = b
+
+        if marker in STANDALONE_MARKERS:
+            print("This is a standalone %02X marker" % marker)
+            return (marker, 0)
         else:
-            fp.read(length) # throw away the result
-        return (marker, length)
-
-def skip_entropy(fp):
-    b = fp.read(1)[0]
-    n = 1
-    while True:
-        if b == 0xff:
-            b2 = fp.read(1)[0]
-            n += 1
-            if b2 == 0x00:
-                # this is just an escaped ff in the entropy data
-                pass
-            elif 0xd0 <= b2 <= 0xd7:
-                # this is a restart marker, skip it
-                pass
+            print("This is non-standalone %02X marker, there should be a length next" % marker)
+            length_bytes = self.fp.read(2)
+            length = struct.unpack('>H', length_bytes)[0]-2
+            if self.fp.seekable:
+                self.fp.seek(length, io.SEEK_CUR)
             else:
-                # this is an actual marker; rewind
-                fp.seek(-2, io.SEEK_CUR)
-                return n
-        b = fp.read(1)[0]
-        n += 1
+                self.fp.read(length) # throw away the result
+            return (marker, length)
+
+    def skip_entropy(self):
+        b = self.fp.read(1)[0]
+        n = 1
+        while True:
+            if b == 0xff:
+                b2 = self.fp.read(1)[0]
+                n += 1
+                if b2 == 0x00:
+                    # this is just an escaped ff in the entropy data
+                    pass
+                elif 0xd0 <= b2 <= 0xd7:
+                    # this is a restart marker, skip it
+                    pass
+                else:
+                    # this is an actual marker; rewind
+                    self.fp.seek(-2, io.SEEK_CUR)
+                    return n
+            b = self.fp.read(1)[0]
+            n += 1
+
+    def read_jpeg(self):
+        segment = self.skip_segment()
+        while segment[0] != MARKER_EOI:
+            print("[%02X] length %d" % segment)
+            if segment[0] == MARKER_EOI:
+                break
+            elif segment[0] == MARKER_SOS:
+                # skip until the next segment
+                entropy_length = self.skip_entropy()
+                print("Skipped %d bytes of entropy data" % entropy_length)
+
+            segment = self.skip_segment()
+
+        return {'size': self.fp.tell()}
 
 def read_jpeg(fp):
-    segment = skip_segment(fp)
-    while segment[0] != MARKER_EOI:
-        print("[%02X] length %d" % segment)
-        if segment[0] == MARKER_EOI:
-            break
-        elif segment[0] == MARKER_SOS:
-            # skip until the next segment
-            entropy_length = skip_entropy(fp)
-            print("Skipped %d bytes of entropy data" % entropy_length)
-
-        segment = skip_segment(fp)
-
-    return {'size': fp.tell()}
+    reader = JpegReader(fp)
+    return reader.read_jpeg()
