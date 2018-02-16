7cf45861d386de5ce7a4296f0c465d6b193e0a2e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2018-02-15 22:06:54 -0300

    Big refactor of waste-counter.py
    It's now wrapped into a class and split into nice methods.
    
    I'm not stat()ing the file anymore. Since I load the whole file into memory
    anyway, I can just look at the len() of the byte buffer.

--- waste-counter.py
+++ waste-counter.py
@@ -21,40 +21,50 @@ def parse_args():
 
     return arg_parser.parse_args()
 
+class JpegScanner:
+    def __init__(self):
+        self.total_data = 0
+        self.total_jpeg = 0
+        self.total_waste = 0
+
+    def scan_dir(self, dir_path):
+        it = os.scandir(dir_path)
+        paths = []
+        for entry in sorted(it, key=lambda e: e.name):
+            if entry.is_file() and entry.name.lower().endswith('.jpg') and not entry.name.endswith("-thumb.jpg"):
+                paths.append(os.path.join(dir_path, entry.name))
+
+        return self.scan_files(paths)
+
+    def scan_files(self, paths):
+        paths = tqdm(paths, unit="files")
+
+        for file_path in paths:
+            with open(file_path, "rb") as f:
+                data = f.read()
+            self.scan_buf(data)
+
+            paths.set_postfix({'waste': util.human_readable_size(self.total_waste)})
+
+    def scan_buf(self, buf):
+        file_size = len(buf)
+        jpeg_size = jpegparser.read_jpeg(buf)['size']
+        wasted_size = file_size - jpeg_size
+
+        self.total_data += file_size
+        self.total_jpeg += jpeg_size
+        self.total_waste += wasted_size
+
 args = parse_args()
 
 if args.subcommand != 'dir':
     raise RuntimeError("???")
 
-dir_path = args.dir
-
-total_data = 0
-total_jpeg = 0
-total_waste = 0
-
-it = os.scandir(dir_path)
-entries = []
-for entry in sorted(it, key=lambda e: e.name):
-    if entry.is_file() and entry.name.lower().endswith('.jpg') and not entry.name.endswith("-thumb.jpg"):
-        entries.append(entry)
-
-bar = tqdm(entries, unit="files")
-for entry in bar:
-    file_path = os.path.join(dir_path, entry.name)
-    file_size = entry.stat().st_size
-    with open(file_path, "rb") as f:
-        jpeg_size = jpegparser.read_jpeg(f.read())['size']
-    wasted_size = file_size - jpeg_size
-
-    #print("%d\t%.2f%%\t%s" % (wasted_size, wasted_size / file_size * 100, file_path))
-
-    total_data += file_size
-    total_jpeg += jpeg_size
-    total_waste += wasted_size
-    bar.set_postfix({'waste': util.human_readable_size(total_waste)})
+scanner = JpegScanner()
+scanner.scan_dir(args.dir)
 
 print()
 
-print("total:   %s" % (util.human_readable_size(total_data)))
-print("real:    %s (%.2f%%)" % (util.human_readable_size(total_jpeg),  total_jpeg  / total_data * 100))
-print("garbage: %s (%.2f%%)" % (util.human_readable_size(total_waste), total_waste / total_data * 100))
+print("total:   %s" % (util.human_readable_size(scanner.total_data)))
+print("real:    %s (%.2f%%)" % (util.human_readable_size(scanner.total_jpeg),  scanner.total_jpeg  / scanner.total_data * 100))
+print("garbage: %s (%.2f%%)" % (util.human_readable_size(scanner.total_waste), scanner.total_waste / scanner.total_data * 100))
